1a6b372c1eb271b83d87d1e45dd68503f012d114
nawfel83 <laklitnawfel1@gmail.com>
2025-05-07 21:11:27 +0200

    fix: clean up code formatting and remove unnecessary comments in factorial and print_arguments scripts

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,14 +2,11 @@
 import sys
 
 def factorial(n):
-	result = 1
-	while n > 1:
-		result *= n
-		n -= 1  # Décrémentation de n pour éviter la boucle infinie
-	return result
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
 
-if len(sys.argv) > 1:
-	f = factorial(int(sys.argv[1]))
-	print(f)
-else:
-	print("Usage: ./factorial.py <number>")
+f = factorial(int(sys.argv[1]))
+print(f)
